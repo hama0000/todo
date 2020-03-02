c5597f281323e0200ce73c628d7b99bfe4b365ab
hama0000 <hirokih3036@gmail.com>
2020-03-02 20:08:15 +0900

    todoアプリの移植

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,53 @@
 $(function () {
-    $('h1').css({
-        color: "blue"
-    });
+
+
 });
+
+const $tbody = $('tbody');
+
+const duration = 750;
+let item = "";
+let id = 1;
+
+$('.btnAdd').on('click', function () {
+
+    const text = $('#inputText').val();
+
+    if (text == '') {
+        return;
+    } else {
+        item += '<tr><td>' + id + '</td><td>' + text + '</td><td><input class="checkbox" type="checkbox"></td><td></td></tr>';
+        $tbody.html(item);
+
+        id++;
+
+        $('#inputText').val('');
+    }
+
+});
+
+$(document).on('click', '.checkbox', function () {
+    $checkTd = $(this).parent('td');
+    $tr = $checkTd.parent('tr');
+
+    if ($(this).prop('checked')) {
+        $checkTd.nextAll('td').html('clear!');
+        $tr.stop(true).animate({
+            backgroundColor: '#ddffdd'
+        }, duration);
+    } else {
+        $checkTd.nextAll('td').html('');
+        $tr.stop(true).animate({
+            backgroundColor: 'white'
+        }, duration);
+    }
+
+
+    /*
+    if ($(this).prop('checked')) {
+        $(this).parent('td').nextAll('td').html('clear');
+    } else {
+        $(this).parent('td').nextAll('td').html('');
+    }*/
+});
+
